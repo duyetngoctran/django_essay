3810c88e315da2fa5b54261f82241d93cf7ebce1
duyetngoctran <duyetngoctran@gmail.com>
2022-12-15 01:23:48 +0700

    update credit func

--- pages/views.py
+++ pages/views.py
@@ -10,19 +10,24 @@ from django.core.mail import send_mail
 def home(request):
   
     if request.method == "POST":
-    
+            
         title = request.POST.get("content")
         email = request.POST.get("email")
         user_id = request.POST.get("user_id")
         student = get_object_or_404(Student, user=request.user)
         essay = Essay.objects.create(student=student, title=title)
-        print(title, email, user_id)
         
+            
         if not title:
             messages.error(request, 'This field is required and cannot be empty')
             return redirect('home')
-         
+        if student.credit < 1:
+            messages.error(request, 'You need more credits')
+            return redirect('home')
+            
+                                 
         messages.success(request, 'Your request has been submitted, we will get back to you soon')
+        student.credit = student.credit - 1
         
     return render(request, 'pages/home.html')
 
